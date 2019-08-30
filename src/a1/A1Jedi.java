b12562b9dd229eceac58d0967a5b9ffd41046506
branmkam <branmkam@live.unc.edu>
2019-08-30 15:16:34 -0400

    custString/itemString issue resolved in Jedi. Only problem is repeat customers

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -29,30 +29,37 @@ public class A1Jedi {
 		int customers = scan.nextInt();
 		for (int j = 0; j < customers; j++)
 		{
-			double cTotal = 0.0;
+			//double cTotal = 0.0;
 			String fname = scan.next();
 			String lname = scan.next();
 			int cItems = scan.nextInt();
+			
 			for (int k = 0; k < cItems; k++)
 			{
 				int numItems = scan.nextInt();
 				String itemCall = scan.next();
+				
 				for(int l = 0; l < itemNames.size(); l++)
 				{	
 					//boolean alreadyThere = false; //fix boolean placement to prevent repeats
+					System.out.println(itemCall.equalsIgnoreCase(itemNames.get(l)));
 					if(itemCall.equalsIgnoreCase(itemNames.get(l)))
 					{
-						cTotal += prices.get(l)*numItems;
+						//cTotal += prices.get(l)*numItems;
 						
 						int temp = itemCounts.get(l);
+						
 						temp += numItems;
 						itemCounts.set(l, temp);
+						
 						
 						//if(alreadyThere == false)
 						//{
 							int temp2 = custCounts.get(l);
 							temp2 += 1;
 							custCounts.set(l, temp2);
+							
+							//System.out.println(itemCounts);
 							break;
 							//alreadyThere = true;
 						//}
@@ -63,7 +70,8 @@ public class A1Jedi {
 		
 		String custString = "No";
 		String itemString = "";
-				
+		
+			
 		for(int p = 0; p < itemCounts.size(); p++)
 		{	
 			//tells total number of customers who bought the item
@@ -71,11 +79,19 @@ public class A1Jedi {
 			{
 				custString = custCounts.get(p) + "";
 			}
+			else
+			{
+				custString = "No";
+			}
 			
 			//tells total number of items bought
 			if(itemCounts.get(p) != 0)
 			{
 				itemString = itemCounts.get(p) + " ";
+			}
+			else
+			{
+				itemString = "";
 			}
 			System.out.println(custString + " customers bought " + itemString + itemNames.get(p));
 		}
